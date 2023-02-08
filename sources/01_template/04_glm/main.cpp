c870fb03f43524b652d77dbeeba0d19d9bb106c1
worker-Windows <yangpan0822@qq.com>
2023-02-08 10:53:58 +0800

    @edit glm重载流操作符，提取旋转、缩放、平移矩阵

--- sources/01_template/04_glm/main.cpp
+++ sources/01_template/04_glm/main.cpp
@@ -3,16 +3,14 @@
 * 2. 向量 加法 点乘 叉乘 矩阵
 * 3. 2D空间向量的垂直向量 (就是2D向量绕原点旋转90°）
 * 4. 转置矩阵，逆矩阵
+* 5. 从矩阵中提取旋转、缩放、平移矩阵 https://blog.csdn.net/hunter_wwq/article/details/21473519/
 */
 
-#define TEST4
+#define TEST5
 
 #ifdef TEST1
 
-#include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "overloadGLM.h"
 
 // 两个点生成一个坐标系
 glm::mat4 genMatrix(glm::vec3 v0, glm::vec3 v1)
@@ -61,26 +59,19 @@ glm::mat4 genMatrix(glm::vec3 v0, glm::vec3 v1)
 int main()
 {
     auto ret1 = genMatrix({ 1,0,0 }, { 0,1,0 });
-    std::cout << ret1[0].x << ',' << ret1[0].y << ',' << ret1[0].z << '\n';
-    std::cout << ret1[1].x << ',' << ret1[1].y << ',' << ret1[1].z << '\n';
-    std::cout << ret1[2].x << ',' << ret1[2].y << ',' << ret1[2].z << '\n';
+    std::cout << ret1;
 
-    std::cout << "================\n";
+    std::cout << "--------------------------------------\n";
 
     auto ret2 = genMatrix({ 1,0,0 }, { 0,0,1 });
-    std::cout << ret2[0].x << ',' << ret2[0].y << ',' << ret2[0].z << '\n';
-    std::cout << ret2[1].x << ',' << ret2[1].y << ',' << ret2[1].z << '\n';
-    std::cout << ret2[2].x << ',' << ret2[2].y << ',' << ret2[2].z << '\n';
+    std::cout << ret2;
 }
 
 #endif // TEST1
 
 #ifdef TEST2
 
-#include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "overloadGLM.h"
 
 int main()
 {
@@ -100,8 +91,8 @@ int main()
     float ret4 = glm::dot(v21, v22); // 向量的点乘结果是一个数量
     // glm::cross(v21, v22); // glm中2维向量无法使用glm::cross()
 
-    std::cout << "( 2,3 ) + ( 4,5 ) = \t" << ret2.x << ',' << ret2.y << '\n';
-    std::cout << "( 2,3 ) * ( 4,5 ) = \t" << ret3.x << ',' << ret3.y << '\n';
+    std::cout << "( 2,3 ) + ( 4,5 ) = \t" << ret2 << '\n';
+    std::cout << "( 2,3 ) * ( 4,5 ) = \t" << ret3 << '\n';
     std::cout << "( 2,3 ) . ( 4,5 ) = \t" << ret4 << '\n';
 
     std::cout << "-----------------\n";
@@ -112,38 +103,49 @@ int main()
     glm::vec3 ret6 = v32 * v33;
     float ret7 = glm::dot(v32, v33);       // 点乘
 
-    std::cout << "(1, 0, 0) x (0, 0, 1) = \t" << ret5.x << ',' << ret5.y << ',' << ret5.z << '\n';
-    std::cout << "(1, 0, 0) * (0, 0, 1) = \t" << ret6.x << ',' << ret6.y << ',' << ret6.z << '\n';
+    std::cout << "(1, 0, 0) x (0, 0, 1) = \t" << ret5 << '\n';
+    std::cout << "(1, 0, 0) * (0, 0, 1) = \t" << ret6 << '\n';
     std::cout << "(1, 0, 0) . (0, 0, 1) = \t" << ret7 << '\n';
 
     std::cout << "-----------------\n";
 
     // glm::mat4 == glm::mat4x4
-    glm::mat4 m41(1.f, 1.f, 1.f, 1.f,
+    glm::mat4 m41(
+        1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f);
 
-    glm::mat4x4 m42(1.f, 1.f, 1.f, 1.f,
+    glm::mat4x4 m42(
+        1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f,
         1.f, 1.f, 1.f, 1.f);
+
+    std::cout << m41 << m42;
 
     if (m42 == m41)
     {
         std::cout << "glm::mat4 == glm::mat4x4\n";
     }
 
-    glm::mat3 m3(1, 2, 3,
+    std::cout << "-----------------\n";
+
+    glm::mat3 m3(
+        1, 2, 3,
         4, 5, 6,
         7, 8, 9);
 
-    glm::mat3 m4(3, 2, 1,
+    glm::mat3 m4(
+        3, 2, 1,
         6, 5, 4,
         9, 8, 7);
 
     glm::vec3 ret8 = m3 * v31;
     glm::vec3 ret9 = v31 * m3;
+
+    std::cout << "m3 * v31:\n" << ret8 << "v31 * m3:\n" << ret9;
+
     if (ret8 == ret9)
     {
         std::cout << " m3 * v31 == v31 * m3 \n";
@@ -157,10 +159,7 @@ int main()
 
 #ifdef TEST3
 
-#include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "overloadGLM.h"
 
 // type为true则垂直向量在原向量右边，否则在左边
 glm::vec2 GenVec(glm::vec2 vec, int type)
@@ -191,10 +190,7 @@ int main()
 
 #ifdef TEST4
 
-#include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "overloadGLM.h"
 
 int main()
 {
@@ -205,31 +201,119 @@ int main()
         11.f, 13.f, 15.f, 6.f,
         10.f, 9.f, 8.f, 7.f };
 
+    // 逆矩阵 AB=BA=E 则称B是A的逆矩阵
     auto inverseMat = glm::inverse(mat1);
+
+    // 转置矩阵 将矩阵的行列互换
     auto transposeMat = glm::transpose(mat1);
 
-    // 逆矩阵
     auto v2 = inverseMat * v1;
     auto v3 = v1 * inverseMat;
     auto v4 = transposeMat * v1;
     auto v5 = v1 * transposeMat;
 
-    // 转置矩阵
     auto v6 = v2 * transposeMat;
     auto v7 = v3 * transposeMat;
     auto v8 = transposeMat * v2;
     auto v9 = transposeMat * v3;
 
+    auto v10 = v1* mat1* glm::transpose(mat1);
+    auto v11 = mat1* v1* glm::transpose(mat1);
+
+    auto mat2 = mat1 * glm::transpose(mat1);
+    auto mat3 = mat1 * glm::inverse(mat1);
+
     // 1 和 6 以及 9 相等
-    std::cout << v1.x << ',' << v1.y << ',' << v1.z << ',' << v1.w<< '\n';  //
-    std::cout << v2.x << ',' << v2.y << ',' << v2.z << ',' << v2.w<< '\n';
-    std::cout << v3.x << ',' << v3.y << ',' << v3.z << ',' << v3.w << '\n';
-    std::cout << v4.x << ',' << v4.y << ',' << v4.z << ',' << v4.w << '\n';
-    std::cout << v5.x << ',' << v5.y << ',' << v5.z << ',' << v5.w << '\n';
-    std::cout << v6.x << ',' << v6.y << ',' << v6.z << ',' << v6.w << '\n'; //
-    std::cout << v7.x << ',' << v7.y << ',' << v7.z << ',' << v7.w << '\n';
-    std::cout << v8.x << ',' << v8.y << ',' << v8.z << ',' << v8.w << '\n';
-    std::cout << v9.x << ',' << v9.y << ',' << v9.z << ',' << v9.w << '\n'; //
+    std::cout << "----- v1 ----- \n" <<v1<< '\n';  //
+    std::cout << "----- v2 ----- \n" <<v2<< '\n';
+    std::cout << "----- v3 ----- \n" <<v3<< '\n';
+    std::cout << "----- v4 ----- \n" <<v4<< '\n';
+    std::cout << "----- v5 ----- \n" <<v5<< '\n';
+    std::cout << "----- v6 ----- \n" <<v6<< '\n'; //
+    std::cout << "----- v7 ----- \n" <<v7<< '\n';
+    std::cout << "----- v8 ----- \n" <<v8<< '\n';
+    std::cout << "----- v9 ----- \n" <<v9<< '\n'; //
 }
 
 #endif // TEST4
+
+#ifdef TEST5
+
+#include "overloadGLM.h"
+
+int main()
+{
+    double m00 = 1.;
+    double m01 = 2.;
+    double m02 = 3.;
+
+    double m10 = 4.;
+    double m11 = 5.;
+    double m12 = 6.;
+
+    double m20 = 7.;
+    double m21 = 8.;
+    double m22 = 9.;
+
+    double m30 = 3.;
+    double m31 = 6.;
+    double m32 = 9.;
+
+    glm::mat4 resultMat{
+        m00, m10, m20, m30,
+        m01, m11, m21, m31,
+        m02, m12, m22, m32,
+          0,   0,   0,   1,
+    };
+
+    // 每一列的向量长度
+    auto l0 = std::hypot(m00, m01, m02);
+    auto l1 = std::hypot(m10, m11, m12);
+    auto l2 = std::hypot(m20, m21, m22);
+
+    glm::mat4 rotateMat{
+        m00/l0, m10/l1, m20/l2, 0,
+        m01/l0, m11/l1, m21/l2, 0,
+        m02/l0, m12/l1, m22/l2, 0,
+             0,      0,      0, 1,
+    };
+
+    glm::mat4 translationMat{
+        1, 0, 0, m30,
+        0, 1, 0, m31,
+        0, 0, 1, m32,
+        0, 0, 0,   1,
+    };
+
+    glm::mat4 scaleMat{
+        l0,  0,  0, 0,
+         0, l1,  0, 0,
+         0,  0, l2, 0,
+         0,  0,  0, 1,
+    };
+
+    std::cout <<
+        "-----------result mat:--------------\n" << resultMat <<
+        "-----------rotate mat:--------------\n" << rotateMat <<
+        "-----------scale mat:--------------\n" << scaleMat <<
+        "-----------translation mat:--------------\n" << translationMat << '\n';
+
+    // 只有这个resultMat和定义的相同
+    resultMat = scaleMat * rotateMat * translationMat;
+    std::cout << "-----------result mat:--------------\n" << resultMat;
+
+    resultMat = translationMat * scaleMat * rotateMat;
+    std::cout << "-----------result mat:--------------\n" << resultMat;
+
+    resultMat = translationMat * rotateMat * scaleMat;
+    std::cout << "-----------result mat:--------------\n" << resultMat;
+
+    resultMat = rotateMat * scaleMat * translationMat;
+    std::cout << "-----------result mat:--------------\n" << resultMat;
+
+
+    return 0;
+
+}
+
+#endif // TEST5
---
+++ sources/01_template/04_glm/overloadGLM.h
@@ -0,0 +1,47 @@
+﻿
+// glm重载流操作符
+
+//------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+std::ostream& operator<<(std::ostream& os, const glm::vec2& vec)
+{
+    os << vec.x << '\t' << vec.y << '\n';
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec3& vec)
+{
+    os << vec.x << '\t' << vec.y << '\t' << vec.z << '\n';
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::vec4& vec)
+{
+    os << vec.x << '\t' << vec.y << '\t' << vec.z << '\t' << vec.w << '\n';
+    return os;
+}
+
+//------------------------------------------------------------------------------------------------
+
+std::ostream& operator<<(std::ostream& os, const glm::mat3& mat)
+{
+    os << mat[0].x << '\t' << mat[0].y << '\t' << mat[0].z << '\n' <<
+        mat[1].x << '\t' << mat[1].y << '\t' << mat[1].z << '\n' <<
+        mat[2].x << '\t' << mat[2].y << '\t' << mat[2].z << '\n';
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const glm::mat4& mat)
+{
+    os << mat[0].x << '\t' << mat[0].y << '\t' << mat[0].z << '\t' << mat[0].w << '\n' <<
+        mat[1].x << '\t' << mat[1].y << '\t' << mat[1].z << '\t' << mat[1].w << '\n' <<
+        mat[2].x << '\t' << mat[2].y << '\t' << mat[2].z << '\t' << mat[2].w << '\n' <<
+        mat[3].x << '\t' << mat[3].y << '\t' << mat[3].z << '\t' << mat[3].w << '\n';
+    return os;
+}
+
+//------------------------------------------------------------------------------------------------
