4186696bca92b0405981e374393269e90047a34a
worker-Windows <yangpan0822@qq.com>
2023-03-08 15:10:38 +0800

    @edit 透视投影，正交投影 glm::perspective glm::ortho

--- sources/02_learnOpenGL/01_instruction/04_transform/main.cpp
+++ sources/02_learnOpenGL/01_instruction/04_transform/main.cpp
@@ -2,11 +2,12 @@
 /*
  * 1. 使用glm对三角形做最简单的缩放，平移，旋转
  * 2. 一个绕(1,1,0)旋转30°的立方体
- * 3. MVP矩阵的作用
- * 4. mvp矩阵，随时间旋转的立方体，开启深度测试
+ * 3. MVP矩阵的作用，从模型坐标变换到屏幕坐标的过程
+ * 4. MVP矩阵，随时间旋转的立方体，开启深度测试
+ * 5. 透视投影perspective和正交投影ortho
  */
 
-#define TEST4
+#define TEST5
 
 #ifdef TEST1
 
@@ -364,6 +365,8 @@ int main()
 
         program.Use();
 
+        // 注意每个变换的输入坐标和输出坐标
+
         // 模型矩阵  模型坐标->世界坐标
         // 观察矩阵  世界坐标->观察坐标
         // 投影矩阵  观察坐标->裁剪坐标
@@ -530,3 +533,144 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 #endif // TEST4
+
+#ifdef TEST5
+
+#include <array>
+#include <common.hpp>
+#include <stb_image.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    initOpenGL.SetResizeCB(framebuffer_size_callback);
+    ShaderProgram program("resources/02_01_04_TEST2.vs", "resources/02_01_04_TEST2.fs");
+
+    // clang-format off
+    // 8个顶点
+    std::array<GLfloat, 8 * 6> vertices{
+        // pos                  // color
+        -0.5f, -0.5f, 0.5f,     1.0f, 0.0f, 0.0f, // 前左下
+         0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 0.0f, // 前右下
+         0.5f,  0.5f, 0.5f,     0.0f, 0.0f, 1.0f, // 前右上
+        -0.5f,  0.5f, 0.5f,     1.0f, 1.0f, 1.0f, // 前左上
+
+        -0.5f, -0.5f, -.5f,     1.0f, 1.0f, 0.0f, // 后左下
+         0.5f, -0.5f, -.5f,     0.0f, 1.0f, 1.0f, // 后右下
+         0.5f,  0.5f, -.5f,     1.0f, 0.0f, 1.0f, // 后右上
+        -0.5f,  0.5f, -.5f,     0.0f, 0.0f, 0.0f, // 后左上
+    };
+
+    // 6个面，12个三角形
+    std::array<GLuint,6 * 2 * 3> indices{
+        0, 1, 3, // 前
+        1, 2, 3,
+
+        1, 5, 2, // 右
+        5, 6, 2,
+
+        5, 4, 6, // 后
+        4, 7, 6,
+
+        4, 0, 7, // 左
+        0, 3, 7,
+              
+        3, 2, 7, // 上
+        2, 6, 7,
+
+        0, 1, 4, // 下
+        1, 5, 4,
+    };
+    // clang-format on
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    //----------------------------------------------------------------------------------
+
+    // 开启深度测试，默认关闭
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 清除深度缓冲
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        program.Use();
+
+        // 绕(1,1,0)旋转
+        auto m = glm::rotate(glm::mat4(1.0f), static_cast<float>(glfwGetTime()), glm::vec3(1, 1, 0));
+        //auto m = glm::rotate(glm::mat4(1.0f), glm::radians(30.f), glm::vec3(1, 1, 0));
+
+        // 观察矩阵，在(0,0,5)位置处观察，相当于将物体向屏幕里面平移5个单位
+        auto v = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -5));
+
+        // 透视投影
+        // 近远平面需要为正值
+        // 如果远平面小于近平面就相当于反方向观察
+        // z坐标在[4,6]范围内的显示
+        //auto p = glm::perspective(glm::radians(45.0f), 8 / 6.f, 4.f, 6.f);
+
+        // 正交投影
+        // 将观察坐标在x:[-1,1] y:[-1,1] z:[4,6]范围内的显示
+        // 注意z是[4,6]不是[-6,-4]
+        // 注意和视口大小的影响，模型是正方体，绘制出来后看起来可能不是立方体
+        //auto p = glm::ortho(-1.f, 1.f, -1.f, 1.f, 4.f, 6.f);
+        // 避免视口的宽高比和正交平截头体宽高比不一致导致的拉伸
+        constexpr auto width = 800.0f;
+        constexpr auto height = 600.0f;
+        auto p = glm::ortho(-1.f, 1.f, -1.f / (width / height), 1.f / (width / height), 4.f, 6.f);
+
+        program.SetUniformMat4("transform", p * v * m);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    program.DeleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+#endif // TEST5
