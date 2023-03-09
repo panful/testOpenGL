7feac02504fe8f01479260b5ad952e1f7cd034d1
worker-Windows <yangpan0822@qq.com>
2023-03-09 14:16:43 +0800

    @edit glm中向量与矩阵的运算

--- sources/01_template/04_glm/main.cpp
+++ sources/01_template/04_glm/main.cpp
@@ -1,13 +1,13 @@
 ﻿/*
-* 1. 两个点生成一个坐标系，即两点确定一个面
-* 2. 向量 加法 点乘 叉乘 矩阵
-* 3. 2D空间向量的垂直向量 (就是2D向量绕原点旋转90°）
-* 4. 转置矩阵，逆矩阵
-* 5. 从矩阵中提取旋转、缩放、平移矩阵 https://blog.csdn.net/hunter_wwq/article/details/21473519/
-* 6. 变换矩阵：缩放、平移、旋转，投影矩阵：正交、透视，观察（视图）矩阵
-*/
+ * 1. 两个点生成一个坐标系，即两点确定一个面
+ * 2. 向量 加法 点乘 叉乘 矩阵
+ * 3. 2D空间向量的垂直向量 (就是2D向量绕原点旋转90°）
+ * 4. 转置矩阵，逆矩阵
+ * 5. 从矩阵中提取旋转、缩放、平移矩阵 https://blog.csdn.net/hunter_wwq/article/details/21473519/
+ * 6. glm::scale rotate translate ortho perspective lookAt
+ */
 
-#define TEST6
+#define TEST2
 
 #ifdef TEST1
 
@@ -38,22 +38,22 @@ glm::mat4 genMatrix(glm::vec3 v0, glm::vec3 v1)
         s.z = 0.0f;
     }
 
-    //！[0]
+    // ！[0]
     glm::vec3 t = glm::normalize(glm::cross(r, s));
     return { glm::vec4(-s, 0.0f), glm::vec4(r, 0.0f), glm::vec4(-t, 0.0f), glm::vec4(v0.x, v0.y, v0.z, 1.0f) };
     //! [0]
 
     //! [1] 不太正确
-        //s /= std::hypot(s.x, s.y, s.z);
+    // s /= std::hypot(s.x, s.y, s.z);
 
-        ////glm::vec3 t = r * s;
-        //glm::vec3 t(fabs(r.y * s.z - r.z * s.y), fabs(r.z * s.x - r.x * s.z), fabs(r.x * s.y - r.y * s.x));
+    ////glm::vec3 t = r * s;
+    // glm::vec3 t(fabs(r.y * s.z - r.z * s.y), fabs(r.z * s.x - r.x * s.z), fabs(r.x * s.y - r.y * s.x));
 
-        //return glm::mat4(
-        //    r.x, s.x, t.x, 0.0,
-        //    r.y, s.y, t.y, 0.0,
-        //    r.z, s.z, t.z, 0.0,
-        //    v0.x, v0.y, v0.z, 1.0f);
+    // return glm::mat4(
+    //     r.x, s.x, t.x, 0.0,
+    //     r.y, s.y, t.y, 0.0,
+    //     r.z, s.z, t.z, 0.0,
+    //     v0.x, v0.y, v0.z, 1.0f);
     //![1]
 }
 
@@ -61,12 +61,12 @@ int main()
 {
     // 两个正交向量的叉乘会产生一个与这两个向量都垂直的第三个向量，一起可以组成一个直角坐标系
 
-    auto ret1 = genMatrix({ 1,0,0 }, { 0,1,0 });
+    auto ret1 = genMatrix({ 1, 0, 0 }, { 0, 1, 0 });
     std::cout << ret1;
 
     std::cout << "--------------------------------------\n";
 
-    auto ret2 = genMatrix({ 1,0,0 }, { 0,0,1 });
+    auto ret2 = genMatrix({ 1, 0, 0 }, { 0, 0, 1 });
     std::cout << ret2;
 
     return 0;
@@ -78,84 +78,121 @@ int main()
 
 #include "print_glm.h"
 
+// 在glm中 * 既不是点乘也不是叉乘
+// glm中的向量都是列向量
+
 int main()
 {
-    // 在glm中 * 既不是点乘也不是叉乘
-
-    glm::vec3 v31(1, 2, 3);
     // 获取向量的指针
-    float* ret1 = glm::value_ptr(v31);
-    std::cout << "glm::vec3(1, 2, 3) = \t" << ret1[0] << ',' << ret1[1] << ',' << ret1[2] << '\n';
-
-    std::cout << "-----------------\n";
-
-    glm::vec2 v21(2, 3);
-    glm::vec2 v22(4, 5);
-    glm::vec2 ret2 = v21 + v22;
-    glm::vec2 ret3 = v21 * v22;
-    float ret4 = glm::dot(v21, v22); // 向量的点乘结果是一个数量
-    // glm::cross(v21, v22); // glm中2维向量无法使用glm::cross()
-
-    std::cout << "( 2,3 ) + ( 4,5 ) = \t" << ret2 << '\n';
-    std::cout << "( 2,3 ) * ( 4,5 ) = \t" << ret3 << '\n';
-    std::cout << "( 2,3 ) . ( 4,5 ) = \t" << ret4 << '\n';
-
-    std::cout << "-----------------\n";
-
-    glm::vec3 v32(1, 0, 0);
-    glm::vec3 v33(0, 0, 1);
-    glm::vec3 ret5 = glm::cross(v32, v33); // 叉乘
-    glm::vec3 ret6 = v32 * v33;
-    float ret7 = glm::dot(v32, v33);       // 点乘
-
-    std::cout << "(1, 0, 0) x (0, 0, 1) = \t" << ret5 << '\n';
-    std::cout << "(1, 0, 0) * (0, 0, 1) = \t" << ret6 << '\n';
-    std::cout << "(1, 0, 0) . (0, 0, 1) = \t" << ret7 << '\n';
-
-    std::cout << "-----------------\n";
-
-    // glm::mat4 == glm::mat4x4
-    glm::mat4 m41(
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f);
-
-    glm::mat4x4 m42(
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f,
-        1.f, 1.f, 1.f, 1.f);
-
-    std::cout << m41 << m42;
-
-    if (m42 == m41)
     {
-        std::cout << "glm::mat4 == glm::mat4x4\n";
+        glm::vec3 v31(1, 2, 3);
+        float* ptr = glm::value_ptr(v31);
+        std::cout << "glm::vec3(1, 2, 3) = \t" << ptr[0] << ',' << ptr[1] << ',' << ptr[2] << '\n';
     }
 
-    std::cout << "-----------------\n";
+    std::cout << "-------------------------------------------\n";
 
-    glm::mat3 m3(
-        1, 2, 3,
-        4, 5, 6,
-        7, 8, 9);
-
-    glm::mat3 m4(
-        3, 2, 1,
-        6, 5, 4,
-        9, 8, 7);
-
-    glm::vec3 ret8 = m3 * v31;
-    glm::vec3 ret9 = v31 * m3;
-
-    std::cout << "m3 * v31:\n" << ret8 << "v31 * m3:\n" << ret9;
-
-    if (ret8 == ret9)
+    // 2维向量的点乘，叉乘
     {
-        std::cout << " m3 * v31 == v31 * m3 \n";
+        glm::vec2 v21(2, 3);
+        glm::vec2 v22(4, 5);
+        glm::vec2 ret2 = v21 + v22;
+        glm::vec2 ret3 = v21 * v22;
+        float ret4 = glm::dot(v21, v22); // 向量的点乘结果是一个数量
+        // glm::cross(v21, v22); // glm中2维向量无法使用glm::cross()
+
+        std::cout << "( 2,3 ) + ( 4,5 ) = \t" << ret2 << '\n';
+        std::cout << "( 2,3 ) * ( 4,5 ) = \t" << ret3 << '\n';
+        std::cout << "( 2,3 ) . ( 4,5 ) = \t" << ret4 << '\n';
     }
-    glm::mat3 ret10 = m3 * m4;
+
+    std::cout << "-------------------------------------------\n";
+
+    // 3维向量的点乘，叉乘
+    {
+        glm::vec3 v32(1, 0, 0);
+        glm::vec3 v33(0, 0, 1);
+        glm::vec3 ret5 = glm::cross(v32, v33); // 叉乘
+        glm::vec3 ret6 = v32 * v33;
+        float ret7 = glm::dot(v32, v33); // 点乘
+
+        std::cout << "(1, 0, 0) x (0, 0, 1) = \t" << ret5 << '\n';
+        std::cout << "(1, 0, 0) * (0, 0, 1) = \t" << ret6 << '\n';
+        std::cout << "(1, 0, 0) . (0, 0, 1) = \t" << ret7 << '\n';
+    }
+
+    std::cout << "-------------------------------------------\n";
+
+    // glm::mat3和glm::mat3x3等价，glm::mat4和glm::mat4x4等价
+    {
+        glm::mat3 m31(
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9);
+
+        glm::mat3x3 m32(
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9);
+
+        if (m31 == m32)
+        {
+            std::cout << "glm::mat3 == glm::mat3x3\n";
+        }
+
+        std::cout << "m31 :\n"
+                  << m31
+                  << "m32 :\n"
+                  << m32
+                  << '\n';
+    }
+
+    std::cout << "-------------------------------------------\n";
+
+    // 矩阵和向量相乘
+    {
+        glm::mat3 m31(
+            1, 2, 3,
+            2, 3, 4,
+            3, 4, 5);
+
+        glm::vec3 v3(1, 2, 3);
+
+        auto mv3 = m31 * v3;
+        auto vm3 = v3 * m31;
+
+        std::cout << "mat * vec :\n"
+                  << mv3
+                  << "vec * mat :\n"
+                  << vm3
+                  << '\n';
+
+        if (mv3 == vm3)
+        {
+            std::cout << "mat * vec == vec * mat\n";
+        }
+        else
+        {
+            std::cout << "mat * vec != vec * mat\n";
+        }
+    }
+
+    std::cout << "-------------------------------------------\n";
+
+    {
+        glm::mat4 m4(
+            1, 2, 3, 4,
+            2, 3, 4, 5,
+            3, 4, 5, 6,
+            4, 5, 6, 7);
+
+        glm::vec4 v4(1, 2, 3, 4);
+
+        auto mv4 = m4 * v4;
+        auto vm4 = v4 * m4;
+
+        std::cout << mv4 << vm4;
+    }
 
     return 0;
 }
@@ -199,12 +236,13 @@ int main()
 
 int main()
 {
-    glm::vec4 v1{ 1.1,2.2,3.3,0. };
-    glm::mat4 mat1{
+    glm::vec4 v1 { 1.1, 2.2, 3.3, 0. };
+    glm::mat4 mat1 {
         1.f, 2.f, 3.f, 4.f,
         12.f, 16.f, 14.f, 5.f,
         11.f, 13.f, 15.f, 6.f,
-        10.f, 9.f, 8.f, 7.f };
+        10.f, 9.f, 8.f, 7.f
+    };
 
     // 逆矩阵 AB=BA=E 则称B是A的逆矩阵
     auto inverseMat = glm::inverse(mat1);
@@ -222,22 +260,31 @@ int main()
     auto v8 = transposeMat * v2;
     auto v9 = transposeMat * v3;
 
-    auto v10 = v1* mat1* glm::transpose(mat1);
-    auto v11 = mat1* v1* glm::transpose(mat1);
+    auto v10 = v1 * mat1 * glm::transpose(mat1);
+    auto v11 = mat1 * v1 * glm::transpose(mat1);
 
     auto mat2 = mat1 * glm::transpose(mat1);
     auto mat3 = mat1 * glm::inverse(mat1);
 
     // 1 和 6 以及 9 相等
-    std::cout << "----- v1 ----- \n" <<v1<< '\n';  //
-    std::cout << "----- v2 ----- \n" <<v2<< '\n';
-    std::cout << "----- v3 ----- \n" <<v3<< '\n';
-    std::cout << "----- v4 ----- \n" <<v4<< '\n';
-    std::cout << "----- v5 ----- \n" <<v5<< '\n';
-    std::cout << "----- v6 ----- \n" <<v6<< '\n'; //
-    std::cout << "----- v7 ----- \n" <<v7<< '\n';
-    std::cout << "----- v8 ----- \n" <<v8<< '\n';
-    std::cout << "----- v9 ----- \n" <<v9<< '\n'; //
+    std::cout << "----- v1 ----- \n"
+              << v1 << '\n'; //
+    std::cout << "----- v2 ----- \n"
+              << v2 << '\n';
+    std::cout << "----- v3 ----- \n"
+              << v3 << '\n';
+    std::cout << "----- v4 ----- \n"
+              << v4 << '\n';
+    std::cout << "----- v5 ----- \n"
+              << v5 << '\n';
+    std::cout << "----- v6 ----- \n"
+              << v6 << '\n'; //
+    std::cout << "----- v7 ----- \n"
+              << v7 << '\n';
+    std::cout << "----- v8 ----- \n"
+              << v8 << '\n';
+    std::cout << "----- v9 ----- \n"
+              << v9 << '\n'; //
 }
 
 #endif // TEST4
@@ -264,11 +311,23 @@ int main()
     double m31 = 6.;
     double m32 = 9.;
 
-    glm::mat4 resultMat{
-        m00, m10, m20, m30,
-        m01, m11, m21, m31,
-        m02, m12, m22, m32,
-          0,   0,   0,   1,
+    glm::mat4 resultMat {
+        m00,
+        m10,
+        m20,
+        m30,
+        m01,
+        m11,
+        m21,
+        m31,
+        m02,
+        m12,
+        m22,
+        m32,
+        0,
+        0,
+        0,
+        1,
     };
 
     // 每一列的向量长度
@@ -276,49 +335,87 @@ int main()
     auto l1 = std::hypot(m10, m11, m12);
     auto l2 = std::hypot(m20, m21, m22);
 
-    glm::mat4 rotateMat{
-        m00/l0, m10/l1, m20/l2, 0,
-        m01/l0, m11/l1, m21/l2, 0,
-        m02/l0, m12/l1, m22/l2, 0,
-             0,      0,      0, 1,
+    glm::mat4 rotateMat {
+        m00 / l0,
+        m10 / l1,
+        m20 / l2,
+        0,
+        m01 / l0,
+        m11 / l1,
+        m21 / l2,
+        0,
+        m02 / l0,
+        m12 / l1,
+        m22 / l2,
+        0,
+        0,
+        0,
+        0,
+        1,
     };
 
-    glm::mat4 translationMat{
-        1, 0, 0, m30,
-        0, 1, 0, m31,
-        0, 0, 1, m32,
-        0, 0, 0,   1,
+    glm::mat4 translationMat {
+        1,
+        0,
+        0,
+        m30,
+        0,
+        1,
+        0,
+        m31,
+        0,
+        0,
+        1,
+        m32,
+        0,
+        0,
+        0,
+        1,
     };
 
-    glm::mat4 scaleMat{
-        l0,  0,  0, 0,
-         0, l1,  0, 0,
-         0,  0, l2, 0,
-         0,  0,  0, 1,
+    glm::mat4 scaleMat {
+        l0,
+        0,
+        0,
+        0,
+        0,
+        l1,
+        0,
+        0,
+        0,
+        0,
+        l2,
+        0,
+        0,
+        0,
+        0,
+        1,
     };
 
-    std::cout <<
-        "-----------result mat:--------------\n" << resultMat <<
-        "-----------rotate mat:--------------\n" << rotateMat <<
-        "-----------scale mat:--------------\n" << scaleMat <<
-        "-----------translation mat:--------------\n" << translationMat << '\n';
+    std::cout << "-----------result mat:--------------\n"
+              << resultMat << "-----------rotate mat:--------------\n"
+              << rotateMat << "-----------scale mat:--------------\n"
+              << scaleMat << "-----------translation mat:--------------\n"
+              << translationMat << '\n';
 
     // 只有这个resultMat和定义的相同
     resultMat = scaleMat * rotateMat * translationMat;
-    std::cout << "-----------result mat:--------------\n" << resultMat;
+    std::cout << "-----------result mat:--------------\n"
+              << resultMat;
 
     resultMat = translationMat * scaleMat * rotateMat;
-    std::cout << "-----------result mat:--------------\n" << resultMat;
+    std::cout << "-----------result mat:--------------\n"
+              << resultMat;
 
     resultMat = translationMat * rotateMat * scaleMat;
-    std::cout << "-----------result mat:--------------\n" << resultMat;
+    std::cout << "-----------result mat:--------------\n"
+              << resultMat;
 
     resultMat = rotateMat * scaleMat * translationMat;
-    std::cout << "-----------result mat:--------------\n" << resultMat;
-
+    std::cout << "-----------result mat:--------------\n"
+              << resultMat;
 
     return 0;
-
 }
 
 #endif // TEST5
