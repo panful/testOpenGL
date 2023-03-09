d1dba07ec31680aaafdbb2b931feb80ce00a6f2e
worker-Windows <yangpan0822@qq.com>
2023-03-09 17:23:55 +0800

    @edit NDC标准化设备坐标

--- sources/02_learnOpenGL/01_instruction/04_transform/main.cpp
+++ sources/02_learnOpenGL/01_instruction/04_transform/main.cpp
@@ -5,9 +5,10 @@
  * 3. MVP矩阵的作用，从模型坐标变换到屏幕坐标的过程
  * 4. MVP矩阵，随时间旋转的立方体，开启深度测试
  * 5. 透视投影perspective和正交投影ortho
+ * 6. NDC(Normalized Device Coordinates)标准化设备坐标系
  */
 
-#define TEST5
+#define TEST6
 
 #ifdef TEST1
 
@@ -370,7 +371,7 @@ int main()
         // 模型矩阵  模型坐标->世界坐标
         // 观察矩阵  世界坐标->观察坐标
         // 投影矩阵  观察坐标->裁剪坐标
-        // 透视除法  裁剪坐标->标准化设备坐标
+        // 透视除法  裁剪坐标->标准化设备坐标（NDC）
         // 视口变换  标准化设备坐标->屏幕坐标
 
         // 模型矩阵，绕x轴顺时针旋转45°
@@ -674,3 +675,103 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 #endif // TEST5
+
+#ifdef TEST6
+
+#include <array>
+#include <common.hpp>
+#include <stb_image.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    initOpenGL.SetFramebufferSizeCB(framebuffer_size_callback);
+    ShaderProgram program("resources/02_01_04_TEST2.vs", "resources/02_01_04_TEST2.fs");
+
+    // NDC可以看作一个XYZ三个方向都属于[-1,1]范围的立方体
+    // 只有坐标xyz分量都属于这个范围才能显示，其他顶点都会被丢弃
+    // 透视除法的作用就是将裁剪坐标转换为NDC
+    // 裁剪坐标由投影矩阵变换得来
+
+    // 不做【任何变换】的顶点坐标就是【标准化设备坐标系】的坐标
+
+    // clang-format off
+    std::array<GLfloat, 4 * 6> vertices{
+        // pos                  // color
+        -0.5f, -0.5f,  0.0f,     1.0f, 0.0f, 0.0f, // 左下
+         0.5f, -0.5f,  1.5f,     0.0f, 1.0f, 0.0f, // 右下 z大于 1.0，因此会被丢弃
+         0.5f,  0.5f,  0.0f,     0.0f, 0.0f, 1.0f, // 右上
+        -0.5f,  0.5f, -1.5f,     1.0f, 1.0f, 1.0f, // 左上 z小于-1.0，因此会被丢弃
+    };
+
+    std::array<GLuint,2 * 3> indices{
+        0, 1, 3,
+        1, 2, 3,
+    };
+    // clang-format on
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    //----------------------------------------------------------------------------------
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.Use();
+
+        auto resultMat = glm::mat4(1.0f); // 不做任何变换
+        program.SetUniformMat4("transform", resultMat);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    program.DeleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+#endif // TEST6
