a3f70ba4bd479e79c3529d04b24b3a4c43aa9c35
worker-Windows <yangpan0822@qq.com>
2023-03-17 16:01:51 +0800

    @add 调试glfw事件

--- sources/02_learnOpenGL/00_other/02_debug/main.cpp
+++ sources/02_learnOpenGL/00_other/02_debug/main.cpp
@@ -1,59 +1,10 @@
 ﻿/*
-* 01.获取OpenGL版本，双显卡切换 https://blog.csdn.net/hhy321/article/details/122322475
-* 02.调试OpenGL
+* 01.调试OpenGL
 */
 
-#define TEST2
+#define TEST1
 
 #ifdef TEST1
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    const unsigned int SCR_WIDTH = 800;
-    const unsigned int SCR_HEIGHT = 600;
-
-    if (!glfwInit())
-    {
-        return -1;
-    }
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // 设置窗口隐藏
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GLFW", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    printf("OpenGL vendor string: %s\n", glGetString(GL_VENDOR));
-    printf("OpenGL renderer string: %s\n", glGetString(GL_RENDERER));
-    printf("OpenGL version string: %s\n", glGetString(GL_VERSION));
-
-    auto version = glGetString(GL_VERSION);
-    std::cout << "version\tmajor: " << version[0] << ", minor: " << version[2] << '\n';
-
-    glfwTerminate();
-    return 0;
-}
-#endif // TEST1
-
-#ifdef TEST2
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -292,4 +243,4 @@ void APIENTRY glDebugOutput(GLenum source,
     } std::cout << std::endl;
     std::cout << std::endl;
 }
-#endif // TEST2
+#endif // TEST1
