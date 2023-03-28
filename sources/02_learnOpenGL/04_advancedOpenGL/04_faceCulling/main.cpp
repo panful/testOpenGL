b11c156294c76876f62bf89474367f02fafb3a36
worker-Windows <yangpan0822@qq.com>
2023-03-28 21:17:09 +0800

    @add 背面剔除

---
+++ sources/02_learnOpenGL/04_advancedOpenGL/04_faceCulling/main.cpp
@@ -0,0 +1,247 @@
+/*
+ * 1. 背面剔除基础示例
+ * 2. 对平面使用背面剔除
+ */
+
+#define TEST2
+
+#ifdef TEST1
+
+#include <array>
+#include <common.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    initOpenGL.SetFramebufferSizeCB(framebuffer_size_callback);
+    ShaderProgram program("resources/02_04_04_TEST1.vs", "resources/02_04_04_TEST1.fs");
+
+    // clang-format off
+    // 8个顶点
+    std::array<GLfloat, 8 * 6> vertices{
+        // pos                  // color
+        -0.5f, -0.5f, 0.5f,     1.0f, 0.0f, 0.0f, // 前左下
+         0.5f, -0.5f, 0.5f,     1.0f, 0.0f, 0.0f, // 前右下
+         0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, // 前右上
+        -0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, // 前左上
+
+        -0.5f, -0.5f, -.5f,     0.0f, 1.0f, 0.0f, // 后左下
+         0.5f, -0.5f, -.5f,     0.0f, 1.0f, 0.0f, // 后右下
+         0.5f,  0.5f, -.5f,     0.0f, 1.0f, 0.0f, // 后右上
+        -0.5f,  0.5f, -.5f,     0.0f, 1.0f, 0.0f, // 后左上
+    };
+
+    // 6个面，12个三角形
+    std::array<GLuint, 6 * 2 * 3> indices{
+        0, 1, 3, // 前
+        1, 2, 3,
+
+        1, 5, 2, // 右
+        5, 6, 2,
+
+        5, 4, 6, // 后
+        4, 7, 6,
+
+        4, 0, 7, // 左
+        0, 3, 7,
+              
+        3, 2, 7, // 上
+        2, 6, 7,
+
+        0, 1, 4, // 下
+        1, 5, 4,
+    };
+    // clang-format on
+
+    unsigned int plane1VAO;
+    {
+        unsigned int VBO, EBO;
+        glGenVertexArrays(1, &plane1VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(plane1VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+    }
+
+    //----------------------------------------------------------------------------------
+
+    // 开启模板测试
+    glEnable(GL_DEPTH_TEST);
+    // 开启背面剔除
+    glEnable(GL_CULL_FACE);
+
+    // 设置剔除面的类型，默认是背面
+    // GL_BACK：只剔除背向面。
+    // GL_FRONT：只剔除正向面。
+    // GL_FRONT_AND_BACK：剔除正向面和背向面。
+    glCullFace(GL_FRONT);
+
+    // 将顺时针的面定义为正向面
+    // 默认值是 GL_CCW 逆时针的面 Counter-clockwise
+    glFrontFace(GL_CW);
+
+    // @note 只对像立方体、球体这样的封闭形状有效，不要对平面，线等图元使用背面剔除
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto distance = static_cast<float>(glfwGetTime() / 2.);
+        distance = distance >= 3.f ? 2.99f : distance;
+        std::cout << distance << '\n';
+
+        // 将相机一直靠近立方体，直到相机进入立方体内部
+        // 进入立方体内部后会发现背面（绿色面）没有被渲染（关闭背面剔除可以看到渲染）
+        auto modelMat = glm::mat4(1.f);
+        auto viewMat = glm::lookAt(glm::vec3(0.f, 0.f, 3.f - distance), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f));
+        auto projectiongMat = glm::perspective(glm::radians(30.0f), 8 / 6.f, 0.1f, 100.f);
+
+        program.Use();
+        program.SetUniformMat4("transform", projectiongMat * viewMat * modelMat);
+
+        glBindVertexArray(plane1VAO);
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // remember to delete the buffer
+    program.DeleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+#endif // TEST1
+
+#ifdef TEST2
+
+#include <array>
+#include <common.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    initOpenGL.SetFramebufferSizeCB(framebuffer_size_callback);
+    ShaderProgram program("resources/02_04_04_TEST1.vs", "resources/02_04_04_TEST1.fs");
+
+    // clang-format off
+    std::array<GLfloat, 4 * 6> vertices{
+        // pos                  // color
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, // 前左下
+         0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, // 前右下
+         0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, // 前右上
+        -0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, // 前左上
+    };
+
+    std::array<GLuint, 2 * 3> indices{
+        0, 1, 3,
+        1, 2, 3,
+    };
+    // clang-format on
+
+    unsigned int plane1VAO;
+    {
+        unsigned int VBO, EBO;
+        glGenVertexArrays(1, &plane1VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(plane1VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+    }
+
+    //----------------------------------------------------------------------------------
+
+    // 开启模板测试
+    glEnable(GL_DEPTH_TEST);
+    // 开启背面剔除
+    glEnable(GL_CULL_FACE);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // 当矩形转到背面时，将不会显示任何图元
+        auto modelMat = glm::rotate(glm::mat4(1.f), static_cast<float>(glfwGetTime()), glm::vec3(0.f, 1.f, 0.f));
+        auto viewMat = glm::lookAt(glm::vec3(0.f, 0.f, 3.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f));
+        auto projectiongMat = glm::perspective(glm::radians(30.0f), 8 / 6.f, 0.1f, 100.f);
+
+        program.Use();
+        program.SetUniformMat4("transform", projectiongMat * viewMat * modelMat);
+
+        glBindVertexArray(plane1VAO);
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // remember to delete the buffer
+    program.DeleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+#endif // TEST2
