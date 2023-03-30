8778b16421620b921001d506dfafef53141d158a
worker-Windows <yangpan0822@qq.com>
2023-03-30 19:17:46 +0800

    @edit 帧缓冲后处理时，设置纹理环绕方式为 GL_CLAMP_TO_EDGE

--- sources/02_learnOpenGL/04_advancedOpenGL/05_frameBuffers/main.cpp
+++ sources/02_learnOpenGL/04_advancedOpenGL/05_frameBuffers/main.cpp
@@ -9,7 +9,7 @@
  * 8. 对帧缓冲的纹理进行后期处理，核效果
  */
 
-#define TEST7
+#define TEST8
 
 #ifdef TEST1
 
@@ -1536,6 +1536,13 @@ int main()
 
     Texture myTexture("resources/02_04_05_wall.jpg");
     // Texture myTexture("resources/02_04_05_container.jpg");
+
+    // 注意，核在对屏幕纹理的边缘进行采样的时候，由于还会对中心像素周围的8个像素进行采样，其实会取到纹理之外的像素。
+    // 由于环绕方式默认是GL_REPEAT，所以在没有设置的情况下取到的是屏幕另一边的像素，
+    // 而另一边的像素本不应该对中心像素产生影响，这就可能会在屏幕边缘产生很奇怪的条纹。
+    // 为了消除这一问题，我们可以将屏幕纹理的环绕方式都设置为GL_CLAMP_TO_EDGE。
+    // 这样子在取到纹理外的像素时，就能够重复边缘的像素来更精确地估计最终的值了。
+    myTexture.SetWarpParameter(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
 
     //----------------------------------------------------------------------------------
     // FBO帧缓冲对象
