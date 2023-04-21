09e2c525bb4024da6bab0a466a39d574074d4942
worker-Windows <yangpan0822@qq.com>
2023-04-21 17:29:00 +0800

    @edit 聚光灯

--- sources/02_learnOpenGL/02_lighting/05_lightCasters/main.cpp
+++ sources/02_learnOpenGL/02_lighting/05_lightCasters/main.cpp
@@ -2,9 +2,10 @@
  * 1. 平行光源，太阳光
  * 2. 使用键盘左右键切换点光源和平行光源
  * 3. 对点光源设置衰减
+ * 4. 聚光灯，类似手电筒的光源
  */
 
-#define TEST3
+#define TEST4
 
 #ifdef TEST1
 
@@ -551,3 +552,164 @@ int main()
 }
 
 #endif // TEST3
+
+#ifdef TEST4
+
+#include <array>
+#include <common.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); }
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    initOpenGL.SetFramebufferSizeCB(framebuffer_size_callback);
+
+    ShaderProgram cubeProgram("resources/02_02_05_TEST1_map.vs", "resources/02_02_05_TEST4_map.fs");
+
+    // clang-format off
+    std::array<GLfloat, 8 * 6 * 6>  vertices{
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+    };
+    // clang-format on
+
+    unsigned int VAO;
+    {
+        unsigned int VBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(2);
+    }
+
+    // 漫反射贴图
+    Texture texture_diffuse("resources/container.png", 0);
+    // 彩色的镜面光贴图
+    Texture texture_specular("resources/container_specular_colored.png", 1);
+
+    //----------------------------------------------------------------------------------
+    // 相机位置
+    auto viewPos = glm::vec3(0.f, 0.f, 4.f);
+    // 材质的反光度
+    auto materialShininess = 32.f;
+    // 光源位置(聚光灯)，和相机在同一位置
+    auto lightPosition = viewPos;
+    // 光源方向，指向屏幕里面
+    auto lightDirection = glm::vec3(0.f, 0.f, -1.f);
+    // 聚光灯圆锥张开的角度的余弦值
+    auto lightCutOff = glm::cos(glm::radians(5.f));
+    // 光源颜色
+    auto lightColor = glm::vec3(1.f);
+    // 光源的漫反射、环境反射、镜面反射颜色
+    auto diffuseColor = lightColor * glm::vec3(0.5f);
+    auto ambientColor = lightColor * glm::vec3(0.2f);
+    auto specularColor = lightColor * glm::vec3(1.f);
+
+    //----------------------------------------------------------------------------------
+    // 物体距离光源越远，物体的颜色越暗，即越黑越看不清颜色
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        cubeProgram.Use();
+        texture_diffuse.Bind();
+        texture_specular.Bind();
+
+        cubeProgram.SetUniform1i("material.diffuse", 0);
+        cubeProgram.SetUniform1i("material.specular", 1);
+
+        auto model = glm::rotate(glm::mat4(1.f), static_cast<float>(glfwGetTime()), glm::vec3(1.f, 1.f, 0.f));
+        auto view = glm::lookAt(viewPos, glm::vec3(0.f), glm::vec3(0.f, 1.f, 0.f));
+        auto projection = glm::perspective(glm::radians(30.f), 8.f / 6.f, 0.1f, 100.f);
+        cubeProgram.SetUniform3fv("viewPos", viewPos);
+
+        // 设置聚光灯参数
+        cubeProgram.SetUniform3fv("light.position", lightPosition);
+        cubeProgram.SetUniform3fv("light.direction", lightDirection);
+        cubeProgram.SetUniform1f("light.cutOff", lightCutOff);
+
+        cubeProgram.SetUniform3fv("light.ambient", ambientColor);
+        cubeProgram.SetUniform3fv("light.diffuse", diffuseColor);
+        cubeProgram.SetUniform3fv("light.specular", specularColor);
+
+        cubeProgram.SetUniform1f("material.shininess", materialShininess);
+
+        cubeProgram.SetUniformMat4("model", model);
+        cubeProgram.SetUniformMat4("view", view);
+        cubeProgram.SetUniformMat4("projection", projection);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        texture_diffuse.Release();
+        texture_specular.Release();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+
+    cubeProgram.DeleteProgram();
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif // TEST4
---
+++ sources/02_learnOpenGL/02_lighting/05_lightCasters/resources/02_02_05_TEST4_map.fs
@@ -0,0 +1,61 @@
+﻿#version 330 core
+out vec4 FragColor;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
+    float shininess;
+}; 
+
+struct Light {
+    // 聚光灯
+    vec3 position;  // 位置
+    vec3 direction; // 方向
+    float cutOff;   // 聚光灯圆锥角度余弦值
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TextureCoord;
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    vec3 lightDir = normalize(light.position - FragPos);
+    // 【片段到光源的向量】与【聚光灯方向】的夹角的点乘（余弦值）
+    float theta = dot(lightDir, normalize(-light.direction));
+
+    // 如果片段在聚光灯圆锥内部，则正常显示
+    if(theta > light.cutOff)
+    {
+        // ambient
+        vec3 ambient = light.ambient * texture(material.diffuse, TextureCoord).rgb;
+        
+        // diffuse 
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TextureCoord).rgb;
+        
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * texture(material.specular, TextureCoord).rgb;
+
+        vec3 result = ambient + diffuse + specular;
+        FragColor = vec4(result, 1.0);
+    }
+    else
+    {
+        // 如果片段在聚光灯圆锥的外部，则只显示材质的漫反射颜色（没有灯光会比较暗）
+        FragColor = vec4(light.ambient * texture(material.diffuse, TextureCoord).rgb, 1.f);
+    }
+} 
