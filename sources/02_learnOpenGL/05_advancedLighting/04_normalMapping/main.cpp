9f1aeee921fc5353170f541bb98aa7fa900eeaa1
worker-Windows <yangpan0822@qq.com>
2023-06-02 17:31:46 +0800

    @edit 切线空间

--- sources/02_learnOpenGL/05_advancedLighting/04_normalMapping/main.cpp
+++ sources/02_learnOpenGL/05_advancedLighting/04_normalMapping/main.cpp
@@ -1,9 +1,10 @@
 /*
  * 1. 法线贴图(normal mapping)、凹凸贴图(bump mapping)
- * 2. 
+ * 2. 切线空间
+ * 3. 不使用切线空间的错误演示，和TEST2对比
  */
 
-#define TEST1
+#define TEST2
 
 #ifdef TEST1
 
@@ -48,7 +49,7 @@ int main()
     std::vector<GLfloat> vertices{
         // positions        // normals      // texcoords
         -1.f, -1.f, 0.f,    0.f,0.f,1.f,    0.f,0.f,
-         1.f, -1.f, 0.f,    0.f,0.f,0.f,    1.f,0.f,
+         1.f, -1.f, 0.f,    0.f,0.f,1.f,    1.f,0.f,
         -1.f,  1.f, 0.f,    0.f,0.f,1.f,    0.f,1.f,
          1.f,  1.f, 0.f,    0.f,0.f,1.f,    1.f,1.f
     };
@@ -109,3 +110,209 @@ int main()
 }
 
 #endif // TEST1
+
+#ifdef TEST2
+
+#include <common.hpp>
+
+int main()
+{
+    auto lightPos = glm::vec3(0.f, 5.f, 0.f);
+    InitOpenGL initOpenGL(Camera({ 0.f, 5.f, 0.f }, { 0.f, 0.f, -1.f }, { 0.f, 0.f, 0.f }));
+    auto window = initOpenGL.GetWindow();
+    ShaderProgram program("resources/02_05_04_TEST2.vs", "resources/02_05_04_TEST2.fs");
+
+    // clang-format off
+    glm::vec3 pos1(-1.0f,  0.0f, -1.0f);
+    glm::vec3 pos2(-1.0f,  0.0f,  1.0f);
+    glm::vec3 pos3( 1.0f,  0.0f,  1.0f);
+    glm::vec3 pos4( 1.0f,  0.0f, -1.0f);
+
+    glm::vec2 uv1(0.0f, 1.0f);
+    glm::vec2 uv2(0.0f, 0.0f);
+    glm::vec2 uv3(1.0f, 0.0f);  
+    glm::vec2 uv4(1.0f, 1.0f);
+
+    glm::vec3 nm(0.f, 1.f, 0.f);
+
+    glm::vec3 tangent1, bitangent1;
+    glm::vec3 tangent2, bitangent2;
+
+    // triangle 1
+    glm::vec3 edge1 = pos2 - pos1;
+    glm::vec3 edge2 = pos3 - pos1;
+    glm::vec2 deltaUV1 = uv2 - uv1;
+    glm::vec2 deltaUV2 = uv3 - uv1;
+
+    float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+    tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+    tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+    tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+    bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+    bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+    bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+    // triangle 2
+    edge1 = pos3 - pos1;
+    edge2 = pos4 - pos1;
+    deltaUV1 = uv3 - uv1;
+    deltaUV2 = uv4 - uv1;
+
+    f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+    tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+    tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+    tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+    bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+    bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+    bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+    std::vector<GLfloat> vertices{
+        // positions            // normal         // texcoords  // tangent                          // bitangent
+        pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+        pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+        pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+        pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+        pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+        pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+    };
+    // clang-format on
+
+    Renderer renderer(vertices, { 3, 3, 2, 3, 3 });
+    Texture diffuseMap("resources/brickwall.jpg", 0);
+    Texture normalMap("resources/brickwall_normal.jpg", 1);
+
+    //----------------------------------------------------------------------------------
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.Use();
+        program.SetUniformMat4("model", glm::mat4(1.f));
+        program.SetUniformMat4("view", initOpenGL.GetViewMatrix());
+        program.SetUniformMat4("projection", initOpenGL.GetProjectionMatrix());
+        program.SetUniform3fv("viewPos", initOpenGL.GetViewPosition());
+        program.SetUniform3fv("lightPos", lightPos);
+
+        program.SetUniform1i("diffuseMap", 0);
+        program.SetUniform1i("normalMap", 1);
+
+        diffuseMap.Bind();
+        normalMap.Bind();
+        renderer.Draw(GL_TRIANGLES);
+        diffuseMap.Release();
+        normalMap.Release();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif // TEST2
+
+#ifdef TEST3
+
+#include <common.hpp>
+
+// 绘制的面朝向+z还是朝向+y
+// 如果绘制的面不是朝向+z并且不使用切线空间，渲染结果就会出错
+#define PLANE_FACING_Z
+
+int main()
+{
+#ifdef PLANE_FACING_Z
+    auto lightPos = glm::vec3(0.f, 0.f, 5.f);
+    InitOpenGL initOpenGL(Camera({ lightPos.x, lightPos.y, lightPos.z }, { 0.f, 1.f, 0.f }, { 0.f, 0.f, 0.f }));
+#else
+    auto lightPos = glm::vec3(0.f, 5.f, 0.f);
+    InitOpenGL initOpenGL(Camera({ lightPos.x, lightPos.y, lightPos.z }, { 0.f, 0.f, -1.f }, { 0.f, 0.f, 0.f }));
+#endif
+
+    auto window = initOpenGL.GetWindow();
+    ShaderProgram program("resources/02_05_04_TEST3.vs", "resources/02_05_04_TEST3.fs");
+
+    // clang-format off
+#ifdef PLANE_FACING_Z
+    glm::vec3 pos1(-1.0f,  1.0f,  0.0f);
+    glm::vec3 pos2(-1.0f, -1.0f,  0.0f);
+    glm::vec3 pos3( 1.0f, -1.0f,  0.0f);
+    glm::vec3 pos4( 1.0f,  1.0f,  0.0f);
+
+    glm::vec2 uv1(0.0f, 1.0f);
+    glm::vec2 uv2(0.0f, 0.0f);
+    glm::vec2 uv3(1.0f, 0.0f);  
+    glm::vec2 uv4(1.0f, 1.0f);
+
+    glm::vec3 nm(0.f, 1.f, 0.f);
+#else
+    glm::vec3 pos1(-1.0f,  0.0f, -1.0f);
+    glm::vec3 pos2(-1.0f,  0.0f,  1.0f);
+    glm::vec3 pos3( 1.0f,  0.0f,  1.0f);
+    glm::vec3 pos4( 1.0f,  0.0f, -1.0f);
+
+    glm::vec2 uv1(0.0f, 1.0f);
+    glm::vec2 uv2(0.0f, 0.0f);
+    glm::vec2 uv3(1.0f, 0.0f);  
+    glm::vec2 uv4(1.0f, 1.0f);
+
+    glm::vec3 nm(0.f, 1.f, 0.f);
+#endif
+
+    std::vector<GLfloat> vertices{
+        // positions                // normals              // texcoords
+        pos1.x, pos1.y, pos1.z,     nm.x, nm.y, nm.z,       uv1.x, uv1.y,
+        pos2.x, pos2.y, pos2.z,     nm.x, nm.y, nm.z,       uv2.x, uv2.y,
+        pos3.x, pos3.y, pos3.z,     nm.x, nm.y, nm.z,       uv3.x, uv3.y,
+
+        pos1.x, pos1.y, pos1.z,     nm.x, nm.y, nm.z,       uv1.x, uv1.y,
+        pos3.x, pos3.y, pos3.z,     nm.x, nm.y, nm.z,       uv3.x, uv3.y,
+        pos4.x, pos4.y, pos4.z,     nm.x, nm.y, nm.z,       uv4.x, uv4.y,
+    };
+    // clang-format on
+
+    Renderer renderer(vertices, { 3, 3, 2 });
+
+    Texture diffuseMap("resources/brickwall.jpg", 0);
+    Texture normalMap("resources/brickwall_normal.jpg", 1);
+
+    //----------------------------------------------------------------------------------
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.Use();
+        program.SetUniformMat4("model", glm::mat4(1.f));
+        program.SetUniformMat4("view", initOpenGL.GetViewMatrix());
+        program.SetUniformMat4("projection", initOpenGL.GetProjectionMatrix());
+        program.SetUniform3fv("viewPos", initOpenGL.GetViewPosition());
+        program.SetUniform3fv("lightPos", lightPos);
+
+        program.SetUniform1i("diffuseMap", 0);
+        program.SetUniform1i("normalMap", 1);
+
+        diffuseMap.Bind();
+        normalMap.Bind();
+        renderer.Draw(GL_TRIANGLES);
+        diffuseMap.Release();
+        normalMap.Release();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif // TEST3
