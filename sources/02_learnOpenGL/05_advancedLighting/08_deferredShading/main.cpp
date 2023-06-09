800aaa0d352d18b26a7dcd1046041e2c054ed55f
worker-Windows <yangpan0822@qq.com>
2023-06-09 11:10:56 +0800

    @edit 光体积

--- sources/02_learnOpenGL/05_advancedLighting/08_deferredShading/main.cpp
+++ sources/02_learnOpenGL/05_advancedLighting/08_deferredShading/main.cpp
@@ -4,9 +4,10 @@
  * 3. 正向着色
  * 4. 延迟渲染的背景色光照问题
  * 5. 延迟渲染与正向渲染结合
+ * 6. 光体积
  */
 
-#define TEST5
+#define TEST6
 
 #ifdef TEST1
 
@@ -1021,3 +1022,217 @@ int main()
 }
 
 #endif // TEST5
+
+#ifdef TEST6
+
+#include <common.hpp>
+
+constexpr uint32_t windowWidth { 800 };
+constexpr uint32_t windowHeight { 600 };
+
+void DrawCube(const Renderer& cube, const ShaderProgram& shader)
+{
+    glm::mat4 scale, rotate, translate;
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(1.f, 1.f, 1.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(0.f), glm::vec3(1.f, 1.f, 1.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(-3.f, 3.f, 0.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(1.f, 1.f, 1.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(45.f), glm::vec3(1.f, 1.f, 1.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(-3.f, -3.f, 0.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(2.f, 2.f, 2.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(30.f), glm::vec3(1.f, 1.f, 0.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, 0.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(1.f, 1.f, 1.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(0.f), glm::vec3(1.f, 1.f, 0.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(1.f, 1.f, 3.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(1.f, 1.f, 1.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(45.f), glm::vec3(0.f, 1.f, 0.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(1.f, -1.f, 5.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+
+    scale     = glm::scale(glm::mat4(1.f), glm::vec3(1.f, 1.f, 1.f));
+    rotate    = glm::rotate(glm::mat4(1.f), glm::radians(45.f), glm::vec3(1.f, 0.f, 0.f));
+    translate = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -5.f));
+
+    shader.SetUniformMat4("model", translate * rotate * scale);
+    cube.Draw(GL_TRIANGLES);
+}
+
+int main()
+{
+    InitOpenGL init("Deferred Rendering", windowWidth, windowHeight, Camera({ 0.f, 0.f, 20.f }, { 0.f, 1.f, 0.f }, { 0.f, 0.f, 0.f }));
+    auto window = init.GetWindow();
+
+    // clang-format off
+    std::vector<GLfloat> verticesCube {
+        // back face
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+         1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+        -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+        // front face
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+         1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+        -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+        -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+        // left face
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+        // right face
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+         1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+         1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+         1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+        // bottom face
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+         1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+         1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+        -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+        -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+        // top face
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+         1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+         1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+         1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+        -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+        -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+    };
+
+    std::vector<GLfloat> verticesQuad {
+        // pos                 // texCoords
+        -1.f,   1.f,  0.f,     0.f, 1.f,
+        -1.f,  -1.f,  0.f,     0.f, 0.f,
+         1.f,   1.f,  0.f,     1.f, 1.f,
+         1.f,  -1.f,  0.f,     1.f, 0.f,
+    };
+    // clang-format on
+
+    ShaderProgram programGBuffer("resources/02_05_08_TEST1_gbuffer.vs", "resources/02_05_08_TEST1_gbuffer.fs");
+    ShaderProgram programDeferred("resources/02_05_08_TEST1_deferred.vs", "resources/02_05_08_TEST6_deferred.fs");
+
+    Renderer cube(verticesCube, { 3, 3, 2 });
+    Renderer quad(verticesQuad, { 3, 2 });
+
+    Texture woodTex("resources/wood.png");
+
+    //---------------------------------------------------------------------------------------
+    // 三个纹理分别保存位置向量、法向量、颜色和镜面反光度
+    Texture gPosition(windowWidth, windowHeight, GL_RGB16F, GL_RGB, GL_FLOAT, 0);
+    Texture gNormal(windowWidth, windowHeight, GL_RGB16F, GL_RGB, GL_FLOAT, 1);
+    Texture gAlbedoSpec(windowWidth, windowHeight, GL_RGBA, GL_RGBA, GL_FLOAT, 2);
+
+    RenderBufferObject rbo(GL_DEPTH_COMPONENT, windowWidth, windowHeight);
+
+    FrameBufferObject fboQuad;
+    fboQuad.AddAttachment(GL_COLOR_ATTACHMENT0, gPosition);
+    fboQuad.AddAttachment(GL_COLOR_ATTACHMENT1, gNormal);
+    fboQuad.AddAttachment(GL_COLOR_ATTACHMENT2, gAlbedoSpec);
+    fboQuad.AddAttachment(GL_DEPTH_ATTACHMENT, rbo);
+    fboQuad.SetDrawBuffers({ GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 });
+    if (!fboQuad.Check())
+    {
+        std::cerr << "Frame buffer object error\n";
+        return -1;
+    }
+
+    // 八个光源
+    std::vector<std::pair<glm::vec3, glm::vec3>> lights {
+        { glm::vec3(-20.f, -20.f, -20.f), glm::vec3(1.f, 0.f, 0.f) },
+        { glm::vec3(5.f, 5.f, -5.f), glm::vec3(1.f, 1.f, 0.f) },
+        { glm::vec3(-5.f, 5.f, -5.f), glm::vec3(1.f, 1.f, 1.f) },
+        { glm::vec3(5.f, -5.f, -5.f), glm::vec3(1.f, 1.f, 1.f) },
+        { glm::vec3(5.f, 5.f, 5.f), glm::vec3(0.f, 1.f, 0.f) },
+        { glm::vec3(-5.f, -5.f, 5.f), glm::vec3(0.f, 0.f, 1.f) },
+        { glm::vec3(-5.f, 5.f, 5.f), glm::vec3(1.f, 1.f, 1.f) },
+        { glm::vec3(5.f, -5.f, 5.f), glm::vec3(1.f, 1.f, 1.f) },
+    };
+
+    //---------------------------------------------------------------------------------------
+    // 通过计算光体积半径这种方式并不能提高效率，因为GPU是高度并行的，if的所有分支都会运行
+    // 可以通过渲染光体积的球提高渲染效率
+    // 延迟光照(Deferred Lighting)和切片式延迟着色法(Tile-based Deferred Shading)可以更高效地渲染
+
+    while (!glfwWindowShouldClose(window))
+    {
+        fboQuad.Bind();
+        glEnable(GL_DEPTH_TEST);
+        glClearColor(.1f, .2f, .3f, 1.f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        programGBuffer.Use();
+        programGBuffer.SetUniformMat4("view", init.GetViewMatrix());
+        programGBuffer.SetUniformMat4("projection", init.GetProjectionMatrix());
+        programGBuffer.SetUniform1f("specular", 2.f);
+        woodTex.Bind();
+        DrawCube(cube, programGBuffer);
+        woodTex.Release();
+        glDisable(GL_DEPTH_TEST);
+        fboQuad.Release();
+
+        glClearColor(1.0, 1.0, 1.0, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        programDeferred.Use();
+        programDeferred.SetUniform1i("gPosition", 0);
+        programDeferred.SetUniform1i("gNormal", 1);
+        programDeferred.SetUniform1i("gAlbedoSpec", 2);
+        programDeferred.SetUniform3fv("viewPos", init.GetViewPosition());
+        for (size_t i = 0; i < lights.size(); ++i)
+        {
+            const float constant = 1.0f;
+            const float linear = 0.01f;
+            const float quadratic = 0.1f;
+
+            programDeferred.SetUniform3fv("lights[" + std::to_string(i) + "].Position", lights.at(i).first);
+            programDeferred.SetUniform3fv("lights[" + std::to_string(i) + "].Color", lights.at(i).second);
+            programDeferred.SetUniform1f("lights[" + std::to_string(i) + "].Linear", 0.01f);
+            programDeferred.SetUniform1f("lights[" + std::to_string(i) + "].Quadratic", 0.1f);
+
+            // 计算光体积半径
+            const float maxBrightness = std::fmaxf(std::fmaxf(lights.at(i).second.r, lights.at(i).second.g), lights.at(i).second.b);
+            float radius = (-linear + std::sqrt(linear * linear - 4 * quadratic * (constant - (256.f / 5.f) * maxBrightness))) / (2.f * quadratic);
+            programDeferred.SetUniform1f("lights[" + std::to_string(i) + "].Radius", radius);
+        }
+        gPosition.Bind();
+        gNormal.Bind();
+        gAlbedoSpec.Bind();
+        quad.Draw(GL_TRIANGLE_STRIP);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+#endif // TEST6
---
+++ sources/02_learnOpenGL/05_advancedLighting/08_deferredShading/resources/02_05_08_TEST6_deferred.fs
@@ -0,0 +1,60 @@
+#version 330 core
+
+layout (location = 0) out vec4 FragColor;
+
+in vec2 TexCoords;
+
+struct Light{
+    vec3 Position;
+    vec3 Color;
+    float Linear;
+    float Quadratic;
+    float Radius;
+};
+
+uniform sampler2D gPosition;
+uniform sampler2D gNormal;
+uniform sampler2D gAlbedoSpec;
+
+const int NUM_OF_LIGHTS = 8;
+
+uniform Light lights[NUM_OF_LIGHTS];
+uniform vec3 viewPos;
+
+void main()
+{
+    // 从G缓冲中获取数据
+    vec3 FragPos = texture(gPosition, TexCoords).rgb;
+    vec3 Normal = texture(gNormal, TexCoords).rgb;
+    vec3 Albedo = texture(gAlbedoSpec, TexCoords).rgb;
+    float Specular = texture(gAlbedoSpec, TexCoords).a;
+
+    // 环境光
+    vec3 lighting = Albedo * 0.1;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    for(int i = 0; i < NUM_OF_LIGHTS; ++i)
+    {
+        float distance = length(lights[i].Position - FragPos);
+
+        // 如果片段在光体积内才计算光照，否则直接跳过
+        if(distance < lights[i].Radius)
+        {
+            // 漫反射
+            vec3 lightDir = normalize(lights[i].Position - FragPos);
+            vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Albedo * lights[i].Color;
+            
+            // 镜面反射
+            vec3 halfwayDir = normalize(lightDir + viewDir);
+            float spec = pow(max(dot(halfwayDir, Normal), 0.0), 32);
+            vec3 specular = lights[i].Color * spec * Specular;
+
+            // 光照衰减
+            float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
+
+            lighting += (diffuse + specular) * attenuation;
+       }
+    }
+
+    FragColor = vec4(lighting, 1.0);
+}
