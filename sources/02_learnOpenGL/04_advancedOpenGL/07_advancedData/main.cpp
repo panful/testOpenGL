9e2d10e74ade8d1d11fe7c5daf0b2ea6b2233f3f
worker-Windows <yangpan0822@qq.com>
2023-08-30 16:54:41 +0800

    动态修改顶点颜色数据不修改位置数据

--- sources/02_learnOpenGL/04_advancedOpenGL/07_advancedData/main.cpp
+++ sources/02_learnOpenGL/04_advancedOpenGL/07_advancedData/main.cpp
@@ -3,11 +3,12 @@
  * 2. 在指定时刻分批填充数据
  * 3. 从内存拷贝数据到指定缓冲 glMapBuffer
  * 4. 复制缓冲 glCopyBufferSubData
- * 5. 将多个VBO绑定到一个VAO上
+ * 5. 将多个VBO绑定到一个VAO上，顶点位置、颜色等分开使用glBufferData提交，类似glBufferSubData
  * 6. 顶点着色器的顶点属性设置为mat4
+ * 7. 动态更新顶点的颜色属性，但是不更新位置属性
  */
 
-#define TEST6
+#define TEST7
 
 #ifdef TEST1
 
@@ -94,8 +95,6 @@ int main()
         glfwPollEvents();
     }
 
-    
-
     glDeleteVertexArrays(1, &vertexArrayObject);
     glDeleteBuffers(1, &vertexBufferObject);
 
@@ -144,7 +143,7 @@ std::array<GLfloat, 3 * 3> colors {
     0.0f, 0.0f, 1.0f
 };
 // clang-format on
-}
+} // namespace
 
 int main()
 {
@@ -182,8 +181,6 @@ int main()
         glfwPollEvents();
     }
 
-    
-
     glDeleteVertexArrays(1, &vertexArrayObject);
     glDeleteBuffers(1, &vertexBufferObject);
 
@@ -201,24 +198,26 @@ void processInput(GLFWwindow* window)
         // 保证只被填充一次，当然填充多次也不会出错。
         static std::once_flag once_flag;
 
-        std::call_once(once_flag, []() {
-            // 一定要先绑定VAO
-            glBindVertexArray(vertexArrayObject);
+        std::call_once(once_flag,
+            []()
+            {
+                // 一定要先绑定VAO
+                glBindVertexArray(vertexArrayObject);
 
-            glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices.data());
-            glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(colors), colors.data());
+                glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices.data());
+                glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices), sizeof(colors), colors.data());
 
-            // 位置
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-            glEnableVertexAttribArray(0);
-            // 颜色
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(sizeof(GLfloat) * vertices.size()));
-            glEnableVertexAttribArray(1);
+                // 位置
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+                glEnableVertexAttribArray(0);
+                // 颜色
+                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(sizeof(GLfloat) * vertices.size()));
+                glEnableVertexAttribArray(1);
 
-            glBindVertexArray(0);
+                glBindVertexArray(0);
 
-            std::cout << "Fill data successfully\n";
-        });
+                std::cout << "Fill data successfully\n";
+            });
     }
 }
 
@@ -299,8 +298,6 @@ int main()
         glfwPollEvents();
     }
 
-    
-
     glDeleteVertexArrays(1, &vertexArrayObject);
     glDeleteBuffers(1, &vertexBufferObject);
 
@@ -327,22 +324,25 @@ void processInput(GLFWwindow* window)
 
         static std::once_flag once_flag;
 
-        std::call_once(once_flag, [vertices]() {
-            glBindVertexArray(vertexArrayObject);
+        std::call_once(once_flag,
+            [vertices]()
+            {
+                glBindVertexArray(vertexArrayObject);
 
-            // 获取指针
-            void* ptr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-            // 拷贝数据到内存
-            std::copy(vertices.begin(), vertices.end(), static_cast<GLfloat*>(ptr));
-            // 停止映射，返回true表示数据映射成功，false失败
-            auto success = glUnmapBuffer(GL_ARRAY_BUFFER);
+                // 获取指针
+                void* ptr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+                // 拷贝数据到内存
+                std::copy(vertices.begin(), vertices.end(), static_cast<GLfloat*>(ptr));
+                // 停止映射，返回true表示数据映射成功，false失败
+                auto success = glUnmapBuffer(GL_ARRAY_BUFFER);
 
-            glBindVertexArray(0);
+                glBindVertexArray(0);
 
-            std::cout << "Replace data successfully.\n";
-        });
+                std::cout << "Replace data successfully.\n";
+            });
     }
 }
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -454,8 +454,6 @@ int main()
         glfwPollEvents();
     }
 
-    
-
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
@@ -470,6 +468,7 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -556,8 +555,6 @@ int main()
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    
 
     // remember to delete the buffers
 
@@ -723,3 +720,110 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 #endif // TEST6
+
+#ifdef TEST7
+
+#include <array>
+#include <common.hpp>
+#include <mutex>
+#include <random>
+
+void processInput(GLFWwindow* window);
+
+GLuint VAO { 0 };
+GLuint VBO { 0 };
+
+constexpr size_t vertex_size { 3 * 3 }; // 3个vec3
+
+int main()
+{
+    InitOpenGL initOpenGL;
+    auto window = initOpenGL.GetWindow();
+    ShaderProgram program("resources/02_04_07_TEST1.vs", "resources/02_04_07_TEST1.fs");
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertex_size * 2, nullptr, GL_STATIC_DRAW);
+
+    // 位置
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+    // 颜色
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(sizeof(GLfloat) * vertex_size));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    //---------------------------------------------------------------------------
+    // 按下F1之后更新一次颜色数据
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.Use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+float genColor()
+{
+    static std::default_random_engine randomEngine(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
+    std::uniform_real_distribution<float> randomColor(0.f, 1.f);
+
+    return randomColor(randomEngine);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_F1) == GLFW_PRESS)
+    {
+        // clang-format off
+        static std::array<GLfloat, vertex_size> pos {
+            -0.5f,  -0.5f,  0.0f,
+             0.5f,  -0.5f,  0.0f,
+             0.0f,   0.5f,  0.0f,
+        };
+        // clang-format on
+
+        std::vector<GLfloat> color;
+        for (size_t i = 0; i < vertex_size; ++i)
+        {
+            color.emplace_back(genColor());
+        }
+
+        //--------------------------------------------------------------------------------------
+        glBindVertexArray(VAO);
+
+        // 只提交一次顶点位置数据
+        static std::once_flag flag {};
+        std::call_once(flag, []() { glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(GLfloat) * vertex_size, pos.data()); });
+
+        // 每次按下F1都更新一次颜色数据
+        glBufferSubData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertex_size, sizeof(GLfloat) * vertex_size, color.data());
+
+        glBindVertexArray(0);
+    }
+}
+
+#endif // TEST7
